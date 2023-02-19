70472fec9b0887e7600a372b27e6dcceb4b913eb
g-kooner <65183312+g-kooner@users.noreply.github.com>
2023-02-18 17:40:37 -0700

    sign up and login done
    auth routes and user routes done

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ import { fileURLToPath } from "url";
 import path from "path";
 // register endpoint
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+//import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+//import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import { create } from "domain";
 
 /*CONFIGURATIONS
 -----------------
@@ -58,13 +63,15 @@ auth route = upload picture directly into assests folder (middleware)
 DO NOT MOVE AUTH ROUTE INTO ROUTE FILE NEED UPLOAD VARIABLE 
 */
 app.post("/auth/register", upload.single("picture"), register);
+//app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /*ROUTER
 ------------- 
 setup routes for authorization routes endpoints
 */
 app.use("/auth", authRoutes);
-
+app.use("/users", userRoutes);
+//app.use("/posts", postRoutes);
 /*
 MONGOOSE SETUP 
 ---------------
